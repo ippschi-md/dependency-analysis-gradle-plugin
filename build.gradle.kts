1e3587a2d0476d396d05502f11e2b811606a2785
Tony Robalik <tony.robalik@gmail.com>
2024-11-15 11:17:24 -0800

    experimenting with compression strategies

--- build.gradle.kts
+++ build.gradle.kts
@@ -116,6 +116,8 @@ dependencies {
   implementation(libs.guava)
   implementation(libs.kotlin.stdlib.jdk8)
   implementation(libs.kotlin.editor.relocated)
+  // implementation(libs.kryo)
+  implementation(libs.kryo5)
   implementation(libs.moshi.kotlin)
   implementation(libs.moshix.sealed.reflect)
   implementation(libs.okio)
